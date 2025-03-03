d085d50f37623e4e51a81a18c72c4ed33f68d85c
Edgar Castro <edgarantoniocastroortiz@gmail.com>
2025-03-03 16:23:37 -0400

    se agrego el arrai y la fincion agregarAmigo y sortearAmigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeamigo = []
+
+
+
+
+function agregarAmigo() {
+    
+    const miAmigo = document.getElementById("amigo").value
+   
+    if(miAmigo == "") {
+        alert("ingrese un nombre porfavor");
+    }else{
+        listaDeamigo.push(miAmigo);
+    }
+    
+    
+           
+       
+    
+        
+        
+
+
+    document.getElementById("amigo").value = "";
+
+    
+      console.log(listaDeamigo);
+    
+    
+
+}
+
+function sortearAmigo(){
+  const sorteo = listaDeamigo[Math.floor(Math.random() * listaDeamigo.length)];
+   console.log(sorteo);
+}
+id="listaAmigos"
+id="resultado" 
